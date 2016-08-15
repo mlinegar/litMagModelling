6d8e257b0e572020ad25ed361545dd34c2e79cb4
Mitchell Linegar <mlinegar@reed.edu>
2016-08-15 01:58:28 -0700

    Removed data files that weren't working

--- R/xmlData-data.R
+++
@@ -1,9 +0,0 @@
-#' Loads the text of XML files for each issue of Blast, Egoist, Freewoman, New Freewoman, and Poetry Magazine.
-#' 
-#' This metadata can be found at https://sourceforge.net/projects/mjplab/files/
-#' @docType data
-#' @usage data(xmlData)
-#' @format a data.frame object
-#' @keywords datasets
-#' @source \href{https://sourceforge.net/projects/mjplab/files/}{Modern Journals Project}
-"xmlData"
