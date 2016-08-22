1f86a702df4c3bf757d731cd0b730e0857b85181
Mitchell Linegar <mlinegar@reed.edu>
2016-08-22 15:27:59 -0700

    Simplified the package; there are now three major functions: prep_corpus, make_model, modelToDataframe, and two graphing functions.

---
+++ R/makeLDAvis.R
@@ -0,0 +1,29 @@
+#' Easy interface to LDAvis, only requires a trained topic.model as input
+#'
+#' @param topic.model Mallet-object a trained topic.model from MALLET
+#' @param outDir character directory to store html/js/json files
+#' @param openBrowser logical should R open a browser to create visualizations?
+#' @param asGist logical should the vis be uploaded as a gist? Requires gistr
+#' @param ... arguments passed onto gistr::gist_create
+#' @return html/js/json files of a LDAvis visualization
+#' @export
+
+makeLDAvis <- function(topic.model, outDir = tempfile(), openBrowser = TRUE, asGist = FALSE, ...){
+  phi <- mallet.topic.words(topic.model, smoothed = TRUE, normalized = TRUE)
+  theta <- mallet.doc.topics(topic.model, smoothed = TRUE, normalized = TRUE)
+  doc.length <- rowSums(mallet.doc.topics(topic.model, smoothed = FALSE, normalized = FALSE))
+  word.freqs <- mallet.word.freqs(topic.model)
+  vocab <- topic.model$getVocabulary()
+  json <- list(
+    phi = phi, theta = theta, doc.length = doc.length, vocab = vocab,
+    term.frequency = droplevels(word.freqs)$term.freq)
+  jsonLDA <- LDAvis::createJSON(phi = json$phi, theta = json$theta, doc.length = json$doc.length,
+                                vocab = json$vocab, term.frequency = json$term.frequency)
+  # if you want to put it on Github, need to havae gistr installed
+  # this can be done by:
+  # devtools::install_github('rOpenSci/gistr')
+  # library(gistr)
+  # see help("serVis") for more details
+  if(asGist==TRUE) library(gistr)
+  LDAvis::serVis(jsonLDA, out.dir = outDir, open.browser = openBrowser, as.gist = asGist, ... = ...)
+}
